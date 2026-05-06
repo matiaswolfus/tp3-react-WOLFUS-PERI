e812889ce0316d3eece257858a2df7266fc196b5
MATÍAS WOLFUS <49194355@ort.edu.ar>
2026-05-06 10:16:46 -0300

    añadido el useeffect

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Listado from './components/Listado'
 import Formulario from './components/Formulario/Index'
 
 function App() {
-  const [citas, setCitas] = useState([])
+  const citasGuardadas = localStorage.getItem("citas") ? JSON.parse(localStorage.getItem('citas')) : []
+  const [citas, setCitas] = useState(citasGuardadas)
 
   const agregarCita = (cita) => {
     setCitas([...citas, cita])
@@ -13,6 +14,11 @@ function App() {
   const eliminarCita = (id) => {
     setCitas(citas.filter(cita => cita.id !== id))
   }
+
+  useEffect(() => {
+    localStorage.setItem('citas', JSON.stringify(citas))
+  }, [citas])
+
 
   return (
     <div className="container">
